7d7ae8cab1319462efbc552776e50499ef16f582
Jauhariq <10210048@bsi.ac.id>
2022-12-12 00:49:20 +0700

    first commit

---
+++ application/views/error.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Matrialy | 404</title>
+	<style type="text/css">
+	/* Set height to 100% for body and html to enable the background image to cover the whole page: */
+
+	body, html {
+		height: 100%;
+		margin: 0;
+	}
+
+	.bgimg {
+		/* Background image */
+		background-image: url('<?php echo base_url("/assets/gambar/error.png") ?>');
+		/* Full-screen */
+		height: 100%;
+		/* Center the background image */
+		background-position: center;
+		/* Scale and zoom in the image */
+		background-size: cover;
+		/* Add position: relative to enable absolutely positioned elements inside the image (place text) */
+		position: relative;
+		/* Add a white text color to all elements inside the .bgimg container */
+		color: white;
+		/* Add a font */
+		font-family: "Courier New", Courier, monospace;
+		/* Set the font-size to 25 pixels */
+		font-size: 25px;
+
+	}
+
+</style>
+</head>
+<body>
+	<div class="bgimg">
+	</div>
+</body>
+</html>
